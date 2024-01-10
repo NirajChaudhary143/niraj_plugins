84fb67c86831b9b6a1a73d16eb468c6bef5d26c1
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-01-10 10:14:22 +0545

    task1

---
+++ hello-world/hello-world.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Hello World
+ * Description: I am testing
+ * Version: 1.0.0
+ * Author: Niraj Chaudhary
+ * Author URI: #
+ */
+
+
+if (!defined("ABSPATH")) {
+    header('Location: /ThemeGrill/task3');
+}
+
+
