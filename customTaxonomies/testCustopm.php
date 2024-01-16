dabb7f505cf23db1c873f8537eb538161c3cab8e
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-01-16 08:44:39 +0545

    task10

---
+++ customTaxonomies/testCustopm.php
@@ -0,0 +1,23 @@
+<?php
+class TestCustom
+{
+    function cts_movie_custom_test()
+    {
+        register_post_type('cts_fruits_test', array(
+            'labels' => array(
+                'menu_name' => 'Fruits Test',
+                'name' => 'Fruit Test',
+                'singular_name' => 'Fruit Test',
+                'all_items' => 'All Fruits Test',
+                'add_new_item' => 'Add New Fruit Test',
+                'edit_item' => 'Edit Fruit Test',
+                'add_new' => 'Add New Fruit Test'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'menu_icon' => 'dashicons-carrot',
+            'taxonomies' => array('cts_fruits_taxonomy', 'post_tag', 'category')
+
+        ));
+    }
+}
