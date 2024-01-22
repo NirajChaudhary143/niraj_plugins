352efcc55c9fb2ea365cec33411019fcb37b9ede
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-01-22 17:19:45 +0545

    divide-code-updated

--- user-manage/includes/class-um-form-render.php
+++ user-manage/includes/class-um-form-render.php
@@ -13,9 +13,9 @@ class UM_Form_Render
 {
     public function __construct()
     {
-        include __DIR__ . '/class-um-table-render.php';
-        include __DIR__ . '/function-um-form-handler.php';
-        include __DIR__ . '/function-um-table-handler.php';
+        include UM_PATH_DIR . 'includes/class-um-table-render.php';
+        include UM_PATH_DIR . 'includes/function-um-form-handler.php';
+        include UM_PATH_DIR . 'includes/function-um-table-handler.php';
         $table_render = new UM_Table_Render();
 
         // Shortcodes
@@ -53,47 +53,7 @@ class UM_Form_Render
         if (!is_user_logged_in()) :
             echo wp_kses_post("<h1>You must logged in first.</h1>");
         else :
-?>
-            <div>
-                <div id="success"></div>
-                <form action="" enctype="multipart/form-data" method="POST">
-                    <?php wp_nonce_field('um_employee_nonce', 'um_employee_nonce'); ?>
-                    <label for=""><?php _e("Full Name", "user-manage") ?></label>
-                    <input type="text" id="fullname">
-                    <div id="error_name" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("Email", "user-manage") ?></label>
-                    <input type="email" id="email">
-                    <div id="error_email" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("Contact Number", "user-manage") ?></label>
-                    <input type="text" id="contact_number">
-                    <div id="error_phone_number" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("Gender", "user-manage") ?></label><br>
-                    <input type="radio" name="gender" value="male"><label for=""><?php _e("Male", "user-manage") ?></label>
-                    <input type="radio" name="gender" value="female"><label for=""><?php _e("Female", "user-manage") ?></label>
-                    <input type="radio" name="gender" value="others"><label for=""><?php _e("Others", "user-manage") ?></label><br>
-                    <div id="error_gender" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("User Bio", "user-manage") ?></label>
-                    <textarea id="user_bio" rows="3"></textarea>
-                    <div id="error_user_bio" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("Employee Status", "user-manage") ?></label>
-                    <select id="employee_status">
-                        <option value="" disabled selected><?php _e("Select The Status", "user-manage") ?></option>
-                        <option value="active"><?php _e("Active", "user-manage") ?></option>
-                        <option value="diactive"><?php _e("Diactivate", "user-manage") ?></option>
-                    </select>
-                    <div id="error_status" style="color: red;">
-                    </div>
-                    <label for=""><?php _e("Profile Image", "user-manage") ?></label><br>
-                    <input type="file" id="image" name="image"><br><br>
-                    <input type="submit" value="Add Employee" id="submit_btn">
-                </form>
-            </div>
-<?php
+            include UM_PATH_DIR . 'templates/employee-form-template.php';
         endif;
         $html = ob_get_clean();
         return $html;
--- user-manage/includes/class-um-table-render.php
+++ user-manage/includes/class-um-table-render.php
@@ -17,41 +17,7 @@ class UM_Table_Render
     public function um_employee_table_template()
     {
         ob_start();
-?>
-        <div>
-            <div id="message">
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th><?php esc_html_e("S.N.", "user-manage") ?></th>
-                        <th><?php esc_html_e('Image', 'user-manage') ?></th>
-                        <th>
-                            <?php esc_html_e('Employee Name', 'user-manage') ?>
-                            <select name="order_emp" id="order_emp_select">
-                                <option value="" disabled selected><?php esc_html_e("Select Order", "user-manage") ?></option>
-                                <option value="ASC"><?php esc_html_e("Ascending", "user-manage") ?></option>
-                                <option value="DESC"><?php esc_html_e("Descending", "user-manage") ?></option>
-                            </select>
-                        </th>
-                        <th id="email_order"><?php esc_html_e('Email', 'user-manage') ?></th>
-                        <th id="contact_number_order"><?php esc_html_e('Contact Number', 'user-manage') ?></th>
-                        <th id="gender_order"><?php esc_html_e('Gender', 'user-manage') ?></th>
-                        <th id="user_bio_order"><?php esc_html_e('User Bio', 'user-manage') ?></th>
-                        <th id="emp_status_order"><?php esc_html_e('Employee Status', 'user-manage') ?></th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <?php if (is_user_logged_in()) : ?>
-                    <tbody id="um_emp_table">
-
-                    </tbody>
-                <?php else : ?>
-                    <h1><?php echo esc_html_e('You Must Log in first', 'user-manage') ?></h1>
-                <?php endif ?>
-            </table>
-        </div>
-<?php
+        include UM_PATH_DIR . 'templates/employee-table-template.php';
         $html = ob_get_clean();
         return $html;
     }
